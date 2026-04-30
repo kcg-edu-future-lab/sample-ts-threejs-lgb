5ed07f1a3fe330aeba048de0bc8b9af2a85d5426
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2026-04-30 12:07:16 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { Suspense } from 'react'
+import './App.css'
+import { Canvas } from '@react-three/fiber'
+import { Gltf, OrbitControls } from "@react-three/drei";
+import { MOUSE } from 'three';
+
+const Model = () => {
+  return <Gltf src="/H301.glb" />;
+};
+
+function App() {
+  return (
+    <div className="canvasContainer">
+      <Canvas camera={{
+    fov: 45, // 視野角
+    position: [-8, 3, 8], // 位置
+  }}
+  shadows={"soft"} // 影を有効化
+  >
+      <ambientLight intensity={1} />
+      <OrbitControls
+        enableZoom={true}
+        mouseButtons={{
+          LEFT: MOUSE.PAN,
+          MIDDLE: MOUSE.DOLLY,
+          RIGHT: MOUSE.ROTATE,
+        }}
+      />
+      <Suspense fallback={null}>
+        <Model />
+      </Suspense>
+  </Canvas>
+    </div>)
+}
+
+export default App
