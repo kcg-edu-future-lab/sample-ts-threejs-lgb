f4af6e77585ccd0918004064d8ab331e3c7077d9
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2026-05-01 19:39:52 +0900

    インデント修正

--- src/App.tsx
+++ src/App.tsx
@@ -4,32 +4,28 @@ import { Canvas } from '@react-three/fiber'
 import { Gltf, OrbitControls } from "@react-three/drei";
 import { MOUSE } from 'three';
 
-const Model = () => {
-  return <Gltf src="/H301.glb" />;
-};
-
 function App() {
   return (
     <div className="canvasContainer">
       <Canvas camera={{
-    fov: 45, // 視野角
-    position: [-8, 3, 8], // 位置
-  }}
-  shadows={"soft"} // 影を有効化
-  >
-      <ambientLight intensity={1} />
-      <OrbitControls
-        enableZoom={true}
-        mouseButtons={{
-          LEFT: MOUSE.PAN,
-          MIDDLE: MOUSE.DOLLY,
-          RIGHT: MOUSE.ROTATE,
+          fov: 45, // 視野角
+          position: [-8, 3, 8], // 位置
         }}
-      />
-      <Suspense fallback={null}>
-        <Model />
-      </Suspense>
-  </Canvas>
+        shadows={"soft"} // 影を有効化
+       >
+        <ambientLight intensity={1} />
+          <OrbitControls
+            enableZoom={true}
+            mouseButtons={{
+              LEFT: MOUSE.PAN,
+              MIDDLE: MOUSE.DOLLY,
+              RIGHT: MOUSE.ROTATE,
+            }}
+        />
+        <Suspense fallback={null}>
+          <Gltf src="/H301.glb" />
+        </Suspense>
+      </Canvas>
     </div>)
 }
 
