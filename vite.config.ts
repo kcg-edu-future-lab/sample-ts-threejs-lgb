4ab8510420e89af0e5596451cce4cc4117309d39
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2026-06-19 21:59:54 +0900

    viteを最新版に変更

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react-swc"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    useAtYourOwnRisk_mutateSwcOptions(options) {
+      options.jsc!.parser!.decorators = true
+      options.jsc!.transform!.decoratorVersion = '2022-03'
+    },
+  })],
   base: "./",
   server:{
     open: true
