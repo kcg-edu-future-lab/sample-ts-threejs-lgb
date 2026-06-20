83049898c15c48118493d9bcebbeb21f2cb11ea1
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2026-06-20 15:27:39 +0900

    plugin-reactに移行

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
-import react from "@vitejs/plugin-react-swc"
+import react from "@vitejs/plugin-react"
+import babel from "@rolldown/plugin-babel"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react({tsDecorators: true})],
+  plugins: [
+    babel({
+      presets: [{
+        preset: () => ({ plugins: [["@babel/plugin-proposal-decorators", { version: "legacy" }]] }),
+        rolldown: { filter: { code: "@" } },
+      }],
+    }),
+    react()
+  ],
   base: "./",
   server:{
     open: true
